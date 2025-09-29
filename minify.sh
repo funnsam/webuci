b214913ace39926e55cc7a88fe40ea5b9033363f
funnsam <funnsam8@gmail.com>
2025-09-29 19:22:54 +0800

    fast

--- minify.sh
+++ minify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find . -path ./node_modules -prune -o -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) -print | while read fname; do
+find . -path ./node_modules -prune -o -path ./.netlify -prune -o -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) -print | while read fname; do
     echo ${fname}
     npm x minify ${fname} > ${fname}.min
     mv ${fname}.min ${fname}
